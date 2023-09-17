aac592ede8bac3b1eae3ad5abb7ead2b6cd3429a
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-17 22:13:45 +0200

    create class Product and add models name image images price

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='peoduct')
+    price = models.FloatField()
+
+
+
+
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE , related_name= 'product_image')
+    image = models.ImageField(upload_to='product_images')
