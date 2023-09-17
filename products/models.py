a2b06c2d698da7119d909126c1744fd76f53d15f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-17 22:49:26 +0200

    add class ProductReviews in models.pc

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -14,3 +16,11 @@ class Product(models.Model):
 class ProductImages(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE , related_name= 'product_image')
     image = models.ImageField(upload_to='product_images')
+
+
+class ProductReviews(models.Model):
+    user = models.ForeignKey(User,related_name='review_user', on_delete=models.SET_NULL,null=True , blank= True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE , related_name= 'review_product')
+    review = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(default= timezone.now)
+    rate = models.IntegerField()
