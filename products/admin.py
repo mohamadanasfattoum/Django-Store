7e173d853fb10a1eabd0d41270f208269f808842
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-19 15:50:56 +0200

    add summernote in admin panle

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, Brand, ProductImages, Review
 
-admin.site.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImages)
 admin.site.register(Brand)
 admin.site.register(Review)
--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
-
+    'django_summernote',
     # my apps
     'products',
 ]
