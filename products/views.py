70935fd6b2259304939f5b4ece171cd0db649917
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-23 09:31:59 +0200

    add get_context_data in views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product , ProductImages
 
 class ProductList(ListView):
     model = Product
@@ -8,3 +9,9 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product
+
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["product-image"] = ProductImages.objects.filter(Product=self.get_object())
+        return context
