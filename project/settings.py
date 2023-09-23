2e9c76a975a050bed2e45f6274e09ac22ef24124
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-24 00:03:58 +0200

    correct path

--- project/settings.py
+++ project/settings.py
@@ -133,4 +133,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#
 APPEND_SLASH = False
