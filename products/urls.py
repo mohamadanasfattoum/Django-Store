a5954c7048b9c7c150ee21da7f86f7a9e0033d4c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-23 18:19:57 +0200

    add company data in base

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,8 @@ from .views import ProductList, ProductDetail, BrandList
 
 urlpatterns = [
     path('' , ProductList.as_view()),
+    path('brands/' , BrandList.as_view()),
     path('<slug:slug>/', ProductDetail.as_view()),
-    path('brand/' , BrandList.as_view()),
+    
 
 ]
--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = False
