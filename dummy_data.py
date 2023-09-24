e2dc511cb5cd64f2b3320ecf47f9b7210925e430
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-24 19:10:14 +0200

    add def add_brands  with name and image in dummy_data

--- dummy_data.py
+++ dummy_data.py
@@ -1,9 +1,16 @@
 from products.models import Product, Brand, Review
 import random
-import faker
+from faker import Faker
 
 def add_brands(n):
-    pass
+    fack = Faker()
+    images = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg']
+    for x in range(n):
+        Brand.objects.create(
+            name = fack.name(),
+            image = f"brands/{images[random.randint(0,9)]}"
+        )
+    print(f"{n} brands was created successfully" )
 
 
 def add_products(n):
@@ -16,5 +23,5 @@ def add_reviews(n):
 
 
 
-
+add_brands(2)
 
