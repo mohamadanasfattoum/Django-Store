a6776d1b6b8deaee2501dc5b8f81ff97de614d03
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-24 20:15:53 +0200

    add dummy products

--- dummy_data.py
+++ dummy_data.py
@@ -8,30 +8,30 @@ import random
 from faker import Faker
 
 def add_brands(n):
-    fack = Faker()
+    fake = Faker()
     images = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg']
     for x in range(n):
         Brand.objects.create(
-            name = fack.name(),
+            name = fake.name(),
             image = f"brands/{images[random.randint(0,9)]}"
         )
     print(f"{n} brands was created successfully" )
 
 
 def add_products(n):
-    fack = Faker()
+    fake = Faker()
     images = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg']
     flags = ['Sale','New','Feature']
     for x in range(n):
         Product.objects.create(
-            name = fack.name(),
+            name = fake.name(),
             image = f"product/{images[random.randint(0,9)]}",
             price = round(random.uniform(20.99,99.99),2),
-            flag = flags[random.randit(0,2)],
-            brand = Brand.objects.get(id=random.randit(1,55)),
+            flag = flags[random.randint(0,2)],
+            brand = Brand.objects.get(id=random.randint(1,55)),
             sku = random.randint(1000,1000000),
-            subtitle = fack.text(max_nb_chars=200),
-            description = fack.text(max_nb_chars=1000),
+            subtitle = fake.text(max_nb_chars=200),
+            description = fake.text(max_nb_chars=1000),
             quantity = random.randint(5,30),
         )
     print(f"{n} Product was created successfully" )
@@ -39,7 +39,7 @@ def add_products(n):
 
 
 def add_reviews(n):
-    fack = Faker()
+    fake = Faker()
     for x in range(n):
         pass
 
@@ -47,4 +47,4 @@ def add_reviews(n):
 
 
 
-add_products(2)
+add_products(1000)
