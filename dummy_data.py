84e8d1d32c4d7f677b2f53bb9f11944246e8db89
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-24 18:29:46 +0200

    add defs add_brands products reviews dummy_dataa

--- dummy_data.py
+++ dummy_data.py
@@ -1,3 +1,20 @@
 from products.models import Product, Brand, Review
 import random
-from faker import Faker
+import faker
+
+def add_brands(n):
+    pass
+
+
+def add_products(n):
+    pass
+
+
+
+def add_reviews(n):
+    pass
+
+
+
+
+
