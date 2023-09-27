154c8e23ae15ec69aefb7362d4087286a5ce6d51
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-27 23:23:56 +0200

    corrected path

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,9 @@ from .views import ProductList, ProductDetail, BrandList ,BrandDetail
 
 
 urlpatterns = [
-    path('brands/<slug:slug>' , BrandDetail.as_view()),
+    path('' , ProductList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
     path('brands/' , BrandList.as_view()),
-    path('' , ProductList.as_view()),
+    path('brands/<slug:slug>' , BrandDetail.as_view()),
     
 ]
