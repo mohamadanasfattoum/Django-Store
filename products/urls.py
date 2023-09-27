ae4cc6baa5c9f11806d4d6e47c0e0d3afd041a35
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-27 20:25:38 +0200

    add template_name in ProductDetail in views and path in urls

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProductList, ProductDetail, BrandList
+from .views import ProductList, ProductDetail, BrandList ,BrandDetail
 
 urlpatterns = [
     path('' , ProductList.as_view()),
     path('brands/' , BrandList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
+    path('brands/<slug:slug>' , BrandDetail.as_view()),
     
 
 ]
