414ed3f9643f01660472624024d06f3c7b566f7f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-28 21:46:34 +0200

    add push

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import ProductList, ProductDetail, BrandList ,BrandDetail
 
 
-
+extra_patterns = [
+    path('brands/<slug:slug>' , BrandDetail.as_view()),
+]
 
 urlpatterns = [
     path('' , ProductList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
     path('brands/' , BrandList.as_view()),
-    path('brands/<slug:slug>' , BrandDetail.as_view()),
+    
     
 ]
