f43b26a7aab13508e503f8cbef2d0b2bc2a797ab
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 21:56:04 +0200

    add def product_list_api in api.py

--- products/api.py
+++ products/api.py
@@ -4,6 +4,10 @@ from .serializers import ProductSerializer
 from .models import Product
 
 
+
+
+@api_view(['GET'])
 def product_list_api(request):
     product = Product.objects.all()
     data = ProductSerializer(Product, many=True).data
+    return Response({'product':product})
