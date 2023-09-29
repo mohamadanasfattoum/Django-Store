f5d38ce247f7aa2d6a254912158d350aa9687245
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 21:58:32 +0200

    add api  product_list_api in url.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList, ProductDetail, BrandList ,BrandDetail
-
+from .api import product_list_api
 
 
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('brands/', BrandList.as_view()),
     path('brands/<slug:slug>', BrandDetail.as_view()),
     
+
+
+    path('api/list', product_list_api)
 ]
