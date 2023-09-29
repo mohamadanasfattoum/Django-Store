ddf36d9600f73095ca12ea9df760b1e81d61adc2
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 19:16:26 +0200

    add new_products in  home.html

--- settings/views.py
+++ settings/views.py
@@ -6,9 +6,10 @@ def home(request):
     brands = Brand.objects.all()[:20]
     sale_products = Product.objects.filter(flag='Sale')[:10]
     featur_products = Product.objects.filter(flag='Feature')[:6]
-
+    new_products = Product.objects.filter(flag='New')[:6]
 
 
     return render(request,'settings/home.html',{
-        'brands':brands, 'sale_products':sale_products, 'featur_products':featur_products
+        'brands':brands, 'sale_products':sale_products, 'featur_products':featur_products,
+        'new_products':new_products
         })
