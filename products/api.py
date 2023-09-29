edd5f3365d4aa99fcb4dc86b0d7678c77e9361f2
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 22:22:18 +0200

    add correct request and push all

--- products/api.py
+++ products/api.py
@@ -9,11 +9,11 @@ from .models import Product
 @api_view(['GET'])
 def product_list_api(request):
     products = Product.objects.all()
-    data = ProductSerializer(products,many=True).data
+    data = ProductSerializer(products,many=True,context ={'request':request}).data
     return Response({'products':data})
 
 @api_view(['GET'])
 def product_detail_api(request,product_id):
     product = Product.objects.get(id=product_id)
-    data = ProductSerializer(product).data
+    data = ProductSerializer(product,context ={'request':request}).data
     return Response({'product':data})
