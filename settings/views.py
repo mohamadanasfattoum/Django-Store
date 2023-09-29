3cc7570febff7e0d2926e73556e7e4c932fdabaf
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 18:42:32 +0200

    add sale_products in home.html

--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from products.models import Product, Brand, Review
 
 
 def home(request):
-    return render(request,'settings/home.html',{})
+    brands = Brand.objects.all()[:20]
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+
+
+
+    return render(request,'settings/home.html',{
+        'brands':brands, 'sale_products':sale_products
+        })
