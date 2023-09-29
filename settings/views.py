65141f6d257e7752d29758ae98afe1222c825feb
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-29 18:55:04 +0200

    add featur_products in  home.html

--- settings/views.py
+++ settings/views.py
@@ -5,9 +5,10 @@ from products.models import Product, Brand, Review
 def home(request):
     brands = Brand.objects.all()[:20]
     sale_products = Product.objects.filter(flag='Sale')[:10]
+    featur_products = Product.objects.filter(flag='Feature')[:6]
 
 
 
     return render(request,'settings/home.html',{
-        'brands':brands, 'sale_products':sale_products
+        'brands':brands, 'sale_products':sale_products, 'featur_products':featur_products
         })
