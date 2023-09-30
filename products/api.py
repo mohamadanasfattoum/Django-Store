8d7864e925cec2eb694a5daa82130224edd4b884
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-30 20:06:10 +0200

    add ProductListAPI

--- products/api.py
+++ products/api.py
@@ -1,3 +1,4 @@
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
@@ -5,7 +6,7 @@ from .models import Product
 
 
 
-
+'''
 @api_view(['GET'])
 def product_list_api(request):
     products = Product.objects.all()
@@ -17,3 +18,8 @@ def product_detail_api(request,product_id):
     product = Product.objects.get(id=product_id)
     data = ProductSerializer(product,context ={'request':request}).data
     return Response({'product':data})
+'''
+
+class ProductListAPI(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
