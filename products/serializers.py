d0c13e33016e4e10f39730ee75e7b3cf21f4e09c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-02 15:07:23 +0200

    show reviews_count

--- products/serializers.py
+++ products/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import Product , Brand
 
 class ProductSerializer(serializers.ModelSerializer):
+    reviews_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
 
+    def get_reviews_count(self,object):
+        reviews_count= object.review_product.all().count()
+        return reviews_count
 
 
 class BrandSerializer(serializers.ModelSerializer):
