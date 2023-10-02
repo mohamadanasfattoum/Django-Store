1160f01a4781cc57ef46d649a2eb237093a1a614
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-02 14:32:58 +0200

    add class BrandSerializer in serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product , Brand
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
         fields = '__all__'
