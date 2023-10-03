70a29f0ad0856294b9774d08b7463e700de7dc04
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-03 22:54:04 +0200

    create app accounts

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     # my apps
     'products',
     'settings',
+    
 ]
 
 MIDDLEWARE = [
