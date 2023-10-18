112142a721eee36052e4fffdaaf51df22def4222
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-18 20:29:14 +0200

    add classes order and orderdetail and add user

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='order_owner' , on_delete= models.SET_NULL , null=True, blank=True)
+    # user >> ForeignKey >> set_null لأن اذا المستخدم سكر حسابه وكان طالب اوردرات لازم تبقى لأن صاحب الموقع بحاجتها   
+    # بس بحال cas بتنحذف بيانات الوردر مع اغلاق العمبل لحسابه
+    # ForeignKey لان علاقة العديد لواحد زبون واحد يطلب عدة اوردرات
+
+class OrderDetail(models.Model):
+    pass
