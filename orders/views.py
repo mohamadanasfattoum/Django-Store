a4f258cea5feb834fe76f40b93848f8305d271a7
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-22 19:28:41 +0200

    add def order_list in views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Order, OrderDetail, Cart,CartDetail, Coupon
 
-# Create your views here.
+
+def order_list(request):
+    order= Order.objects.all()
+    return render(request,'orders/orders.html',{'order':order})
+
+
