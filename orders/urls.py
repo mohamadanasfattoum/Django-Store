7da54c40086e81d87b41824e110bcad1e7d60885
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-22 19:42:11 +0200

    add url.py in orders and add in order.py order_list and checkout

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from .views import order_list, checkout
+
+
+urlpatterns= [
+    path('' , order_list),
+    path('checkout' , checkout),
+
+]
--- project/urls.py
+++ project/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', home),
 ]
