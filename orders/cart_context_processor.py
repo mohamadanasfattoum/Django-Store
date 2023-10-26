5be293962ef6e04335068779f89322170e46889b
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-26 21:18:48 +0200

    add def get_or_create_cart(request) in  cart_context_processor.py

--- orders/cart_context_processor.py
+++ orders/cart_context_processor.py
@@ -1,1 +1,15 @@
 from .models import Cart, CartDetail
+# to show cart in all pages"
+# and to add cart and return that one created it in all pages
+# لأظهار واضافة واعادة اظهار الداتا المصافة في كل صفحة
+
+
+def get_or_create_cart(request): # يا أما تجيب او تنشأ كارت
+    if request.user.is_authenticated: # اذا المستخدم مسجل
+        cart , created = Cart.objects.get_or_create(user=request.user,status='inprogress') # اذا الكارت موجودة اعرصها او غير موجودة انشاء كارت
+        if not created:
+            cart_detail = CartDetail.objects.filter(cart=cart)
+            return {'cart_data':cart, 'cart_detail_data':cart_detail}
+        return {'cart_data':cart}
+    else:
+        return {}
