177d8c92a0299c5e538cc404c984424ee31902f5
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-30 19:46:56 +0100

    add  subtotal and total in checkout

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Order, OrderDetail, Cart,CartDetail, Coupon
+from .models import Order, OrderDetail, Cart, CartDetail, Coupon
 from settings.models import DeliveryFee
 
 def order_list(request):
@@ -11,10 +11,14 @@ def checkout(request):
     cart = Cart.objects.get(user=request.user,status='inprogress')
     cart_detail= CartDetail.objects.filter(cart=cart)
     delivery_fee= DeliveryFee.objects.last().fee
+    sub_total = cart.cart_total()
+    total = sub_total + delivery_fee
 
     return render(request,'orders/checkout.html',{
         'cart':cart ,
         'cart_detail':cart_detail,
         'delivery_fee' : delivery_fee,
+        'sub_total' : sub_total,
+        'total' : total,
 
     })
