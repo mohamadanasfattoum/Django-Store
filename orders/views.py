1590ca448fd97a85e62a85caacfed7631a3d3259
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-30 20:54:53 +0100

    apply coupon in checkout

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
+import datetime
 from django.shortcuts import render
 from .models import Order, OrderDetail, Cart, CartDetail, Coupon
 from settings.models import DeliveryFee
+from django.shortcuts import get_object_or_404
 
 def order_list(request):
     orders= Order.objects.all()
@@ -14,6 +16,35 @@ def checkout(request):
     sub_total = cart.cart_total()
     total = sub_total + delivery_fee
     discount= 0
+
+
+    if request.method == 'POST':
+        code = request.POST['coupon_code'] # هيك بنادي عالكود من الصغحة
+        #coupon = Coupon.objects.get(code=code)
+        coupon = get_object_or_404(Coupon , code=code)    #لكي لايتوقف الموقع عند هدم تطابق الكود
+
+
+        if coupon and coupon.quantity > 0 :  # للتأكد من الوقت 
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                coupon_value = sub_total / 100*coupon.discount
+                sub_total = sub_total - coupon_value
+                total = sub_total + delivery_fee
+
+                cart.coupon = coupon
+                cart.order_total_discount = sub_total # مجموع الخقم الرئيسي
+                cart.save()
+                return render(request,'orders/checkout.html',{
+                    'cart':cart ,
+                    'cart_detail':cart_detail,
+                    'delivery_fee' : delivery_fee,
+                    'sub_total' : round(sub_total,2),
+                    'total' : total,
+                    'discount': round(coupon_value,2),
+
+                })                
+
+
 
     return render(request,'orders/checkout.html',{
         'cart':cart ,
