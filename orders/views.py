a58c1fee2cd050f909bc794019f9a8a3a3655836
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-30 19:50:23 +0100

    add discount as 0

--- orders/views.py
+++ orders/views.py
@@ -13,6 +13,7 @@ def checkout(request):
     delivery_fee= DeliveryFee.objects.last().fee
     sub_total = cart.cart_total()
     total = sub_total + delivery_fee
+    discount= 0
 
     return render(request,'orders/checkout.html',{
         'cart':cart ,
@@ -20,5 +21,6 @@ def checkout(request):
         'delivery_fee' : delivery_fee,
         'sub_total' : sub_total,
         'total' : total,
+        'discount': discount,
 
     })
