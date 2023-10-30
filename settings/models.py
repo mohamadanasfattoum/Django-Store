200810121b16c009c9b6d67ca7253e79cd20e18f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-30 19:17:41 +0100

    add class deiveryfee in models in settings

--- settings/models.py
+++ settings/models.py
@@ -20,3 +20,11 @@ class Company(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class DeliveryFee(models.Model):
+    fee = models.FloatField()
+
+    def __str__(self):
+        return str(self.fee)
+
