1cf7cf6a001c1a6f74886c04fc839050fb4ec7eb
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-31 12:51:32 +0100

    add CartDetailCreateDeleteAPI in urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import order_list, checkout
-
+from .api import CartDetailCreateDeleteAPI
 
 urlpatterns= [
     path('' , order_list),
     path('checkout' , checkout),
 
+
+
+    #api
+    path('api/<str:username>/cart', CartDetailCreateDeleteAPI.as_view()),
 ]
