d29a4f47c80979cfbb8bd3df900ced2c8e7e7ac0
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-31 12:44:50 +0100

    add def get, post, delete  class CartDetailCreateDeleteAPI

--- orders/api.py
+++ orders/api.py
@@ -6,5 +6,14 @@ from django.contrib.auth.models import User
 from .models import Cart, CartDetail, Order, OrderDetail, Coupon
 
 
-class CartDetailCreateDeleteAPI(generics.GenericAPIView):
-    pass
+class CartDetailCreateDeleteAPI(generics.GenericAPIView): # كلاس فارغة استطيع ان اخصص بها ما اشاء GenericAPIViewباستخدام
+    def get():# دالة لاحضار الليس ويتيل
+        pass
+
+
+    def post(): # دالة لحتى اضيف
+        pass
+
+
+    def delete():
+        pass 
