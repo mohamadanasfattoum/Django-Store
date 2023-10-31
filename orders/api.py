77ba7c9731877e8cd8938dde0350be6c6d82847d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-31 13:04:20 +0100

    add get_orcreate cart api

--- orders/api.py
+++ orders/api.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.response import responses
+from rest_framework.response import Response
 from .serializers import CartSerializer, OrderSerializer, CartDetailSerializer, OrderDetailSerializer
 from products.models import Product, Brand
 from django.contrib.auth.models import User
@@ -7,13 +7,19 @@ from .models import Cart, CartDetail, Order, OrderDetail, Coupon
 
 
 class CartDetailCreateDeleteAPI(generics.GenericAPIView): # كلاس فارغة استطيع ان اخصص بها ما اشاء GenericAPIViewباستخدام
-    def get():# دالة لاحضار الليس ويتيل
-        pass
+    def get(self,request,*args,**kwargs):# دالة لاحضار الليس ويتيل
+        
+        user = User.objects.get(username=self.kwargs['username'])
+        cart , created = Cart.objects.get_or_create(user=user,status='inprogress')
+        data = CartDetailSerializer(cart).data #??
+        return Response({'Cart':data})
 
 
-    def post(): # دالة لحتى اضيف
-        pass
+    #def post(): # دالة لحتى اضيف
+        #pass
 
 
-    def delete():
-        pass 
+    #def delete():
+        #pass 
+
+    pass
