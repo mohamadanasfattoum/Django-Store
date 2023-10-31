5f808eda9f4ecfedb85e11f290431565071a1ff8
Mohamad anas fattoum <samaanas30@gmail.com>
2023-10-31 12:31:23 +0100

    add api.py

---
+++ orders/api.py
@@ -0,0 +1,6 @@
+from rest_framework import generics
+from rest_framework.response import responses
+from .serializers import CartSerializer, OrderSerializer, CartDetailSerializer, OrderDetailSerializer
+from products.models import Product, Brand
+from django.contrib.auth.models import User
+from .models import Cart, CartDetail, Order, OrderDetail, Coupon
