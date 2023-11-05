3b1b2b0028e9d92f0eb5972cf22a4e173d9cd750
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-05 17:03:58 +0100

    add CreateOrderAPI, ApplyCouponAPI in urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import order_list, checkout
-from .api import CartDetailCreateDeleteAPI, OrderListAPI, OrderDetailAPI, ApplyCouponAPI
+from .api import CartDetailCreateDeleteAPI, OrderListAPI, OrderDetailAPI, CreateOrderAPI, ApplyCouponAPI
 
 urlpatterns= [
     path('' , order_list),
@@ -10,8 +10,10 @@ urlpatterns= [
 
     #api
     path('api/<str:username>/list', OrderListAPI.as_view()),
+    path('api/<str:username>/order/create', CreateOrderAPI.as_view()),    
     path('api/<str:username>/list/<int:pk>', OrderDetailAPI.as_view()),
     path('api/<str:username>/cart', CartDetailCreateDeleteAPI.as_view()),
+    path('api/<str:username>/cart/apply-coupon', ApplyCouponAPI.as_view()),
 
 
 
