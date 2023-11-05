b13bf49f1ebbe79607fe687a75182a39c55641d9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-05 14:52:28 +0100

    add OrderListAPI in urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import order_list, checkout
-from .api import CartDetailCreateDeleteAPI
+from .api import CartDetailCreateDeleteAPI, OrderListAPI, OrderDetailAPI, ApplyCouponAPI
 
 urlpatterns= [
     path('' , order_list),
@@ -9,5 +9,13 @@ urlpatterns= [
 
 
     #api
+    path('api/<str:username>/list', OrderListAPI.as_view()),
     path('api/<str:username>/cart', CartDetailCreateDeleteAPI.as_view()),
+
+
+
+
+
+
+
 ]
