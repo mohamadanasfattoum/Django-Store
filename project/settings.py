fe8c23b39fef3fb22b7e9675fee8c8fa029c2706
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-14 14:32:05 +0100

    add LocaleMiddleware  in settings

--- project/settings.py
+++ project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",  # لتفعيل زر الغو مشانوتبديل الترجمة
     'django.middleware.common.CommonMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
