b45d8e4619abb28f9d0f514c976ac4cafc059044
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-18 14:04:20 +0100

    push all

--- project/celery.py
+++ project/celery.py
@@ -16,3 +16,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+
+# broker_connection_retry_on_startup = True
+
