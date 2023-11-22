3a26c34568bc9a5dc2960f8e045fb10db39b0e9f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-22 19:00:22 +0100

    desigen profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from utils.generate_code import generate_code
+from django.contrib.auth.models import User 
 
-# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'accounts')
+    code = models.CharField(max_length=10 , default= generate_code)
