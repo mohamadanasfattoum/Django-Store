c0d29be04b488080f93fdc3075d927c44af3fef9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-26 21:25:22 +0100

    add class ContactNumbers in models in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,19 @@ class Profile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     image = models.ImageField(upload_to = 'accounts')
     code = models.CharField(max_length=10 , default= generate_code)
+
+
+NUMBER_TYPS = (
+    ('Primary','Primary') ,
+    ('Secondary','Secondary')
+     )
+
+
+class ContactNumbers(models.Model):
+    user = models.ForeignKey(User, related_name='user_phones', on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=NUMBER_TYPS)
+    number = models.CharField(max_length=10)
+
+
+class Address(models.Model):
+    pass
