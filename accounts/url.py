a4b9c9be1f83d5bf9b8b9b6b232476d1e0c947d3
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-27 19:41:06 +0100

    update urls.py

--- accounts/url.py
+++ accounts/url.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from .views import singup, activate
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('singup', singup),
+    path('<str:username>/activate', activate),
+]
