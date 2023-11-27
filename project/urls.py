b2973c8d2ebd326aaeffc52db37987c7350998d8
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-27 20:29:50 +0100

    update urls.py in project

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
