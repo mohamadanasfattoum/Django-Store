768b57682a33769752453114e64a66b42a916419
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-27 19:38:50 +0100

    add def singup in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm, ActivationForm
+from django.contrib.auth.models import User
+from .models import Profile
+from django.core.mail import send_mail
 
-# Create your views here.
+
+def singup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            username= form.cleaned_data('username')
+            email= form.cleaned_data('email')
+            form.save()
+            # profile----> code
+            profile = Profile.objects.get(user__username=username)
+            send_mail(
+                "Activate Your Account",
+                f"Welcome {username} \nuse this code {profile.code} to activate your account",
+                "samaanas30@gmail.com",
+                [email],
+                fail_silently=False,
+            )
+            return redirect(f'/accounts/{username}/activate')
+    else:
+        form = SignupForm()
+    return render (request, 'registration/signup.html', {'form'=form})
+    
+
+
+def activate(request):
+    pass
