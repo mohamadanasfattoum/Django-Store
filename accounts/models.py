84938a50d4ac6296ff9f30d945c9401be0480486
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-27 12:45:11 +0100

    add def create_profile in models

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,28 @@ from django.db import models
 from utils.generate_code import generate_code
 from django.contrib.auth.models import User 
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     image = models.ImageField(upload_to = 'accounts')
     code = models.CharField(max_length=10 , default= generate_code)
+
+
+
+@receiver(post_save,sender=User) # لم اليوسر ينعمله تسجيل جديد ويتسيف  بتناده عليها
+def create_profile(sender,instance,created,**kwargs): # مين ارسل الاشارةsender. instance المستخدم.  ceared  هل المستخدم نشأ ولالأ
+    if created:# if true لسى عامل الحساب  or fulse   اليوسر يعمل تعديل
+        Profile.objects.get(
+            user= instance
+
+
+        )
+
+
 
 
 NUMBER_TYPS = (
