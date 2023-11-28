ccc3924fc2f29e1fad7b1c1a37abc51a80ca4f61
Mohamad anas fattoum <samaanas30@gmail.com>
2023-11-28 17:13:41 +0100

    fix urls.py and form. and updeat def activate

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,12 @@ def singup(request):
         if form.is_valid():
             username= form.cleaned_data('username')
             email= form.cleaned_data('email')
+
+            user = form.save(commit=False)
+            user.is_active = False
+
+
+
             form.save()
             # profile----> code
             profile = Profile.objects.get(user__username=username)
@@ -25,9 +31,27 @@ def singup(request):
             return redirect(f'/accounts/{username}/activate')
     else:
         form = SignupForm()
-    return render (request, 'registration/signup.html', {'form'=form})
+    return render (request, 'registration/signup.html',  {'form':form})
     
 
 
-def activate(request):
-    pass
+def activate(request,username):
+    profile = Profile.objects.get(user__username=username)
+    if request.method == 'POST':
+        form = ActivationForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            if code == profile.code:
+                profile.code= ''
+
+                user = User.objects.get(username=profile.user.username) # لتفعيل الحساب لما يتأكد من الكود ويساوي اكتف
+                user.is_active = True
+                user.save()
+                profile.save()
+
+                return redirect('/accounts/login')
+
+
+    else:
+        form = ActivationForm()
+    return render (request, 'registration/activate.html', {'form':form})
