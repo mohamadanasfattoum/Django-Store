fab95e3ed574ab943d98af5611715db08fc55667
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-14 14:49:07 +0100

    fix env in settings

--- project/settings.py
+++ project/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+from environ import Env
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b9+t(6(m9xymh(q^9#(f*68hpuzs_b)q!ktm5mzqb%noza_38v'
+
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
@@ -119,9 +118,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "db_store",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
         "HOST": "db",
         "PORT": "5432",
     }
