bca4a77fc1ec392005a5950cd2105cb03f1a264c
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-28 20:31:05 +0100

    fix in profile_context_processor.py

--- accounts/profile_context_processor.py
+++ accounts/profile_context_processor.py
@@ -2,5 +2,9 @@ from .models import Profile
 
 
 def get_accounts_data(request):
-    accounts_data = Profile.objects.last()
+    accounts_data = Profile.objects.get(user=request.user)
     return {'accounts_data':accounts_data }
+
+
+
+
