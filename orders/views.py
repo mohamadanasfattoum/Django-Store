2661fef05075c5f3b2f24e585eb0b4480ed501ec
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-08 22:12:10 +0100

    add round , 2 to total in checkout

--- orders/views.py
+++ orders/views.py
@@ -24,88 +24,13 @@ def order_list(request):
     return render(request,'orders/orders.html',{'orders':orders})
 
 
-
-# @login_required
-# def checkout(request):
-#     cart = Cart.objects.get(user=request.user, status='inprogress')
-#     cart_detail = CartDetail.objects.filter(cart=cart)
-#     delivery_fee = DeliveryFee.objects.last().fee
-#     sub_total = cart.cart_total()
-#     total = sub_total + delivery_fee
-#     discount = 0
-#     pub_key = env('STRIP_API_KEY_PUBLISHABLE')
-
-#     if request.method == 'POST':
-#         code = request.POST['coupon_code']
-#         coupon = get_object_or_404(Coupon, code=code)
-
-#         if coupon and coupon.quantity > 0:
-#             today_date = datetime.datetime.today().date()
-#             if today_date >= coupon.start_date and today_date <= coupon.end_date:
-#                 coupon_value = sub_total / 100 * coupon.discount
-#                 sub_total = sub_total - coupon_value
-#                 total = sub_total + delivery_fee
-
-#                 cart.coupon = coupon
-#                 cart.order_total_discount = sub_total
-#                 coupon.quantity -= 1
-#                 cart.save()
-#                 coupon.save()
-
-#                 if request.headers.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-#                     # Return JSON response for AJAX request
-#                     return JsonResponse({
-#                         'cart': cart.id,
-#                         'cart_detail': cart_detail,
-#                         'delivery_fee': delivery_fee,
-#                         'sub_total': round(sub_total, 2),
-#                         'total': total,
-#                         'discount': round(coupon_value, 2),
-#                         'pub_key': pub_key,
-#                     })
-
-#                 else:
-#                     return render(request, 'orders/checkout.html', {
-#                         'cart_detail': cart_detail,
-#                         'delivery_fee': delivery_fee,
-#                         'sub_total': round(sub_total, 2),
-#                         'total': total,
-#                         'discount': round(coupon_value, 2),
-#                         'pub_key': pub_key,
-#                     })
-
-#     if request.headers.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-#         # Return JSON response for AJAX request
-#         return JsonResponse({
-#             'cart': cart.id,
-#             'cart_detail': cart_detail,
-#             'delivery_fee': delivery_fee,
-#             'sub_total': sub_total,
-#             'total': total,
-#             'discount': discount,
-#             'pub_key': pub_key,
-#         })
-#     else:
-#         # Render HTML for standard HTTP request
-#         return render(request, 'orders/checkout.html', {
-#             'cart': cart,
-#             'cart_detail': cart_detail,
-#             'delivery_fee': delivery_fee,
-#             'sub_total': sub_total,
-#             'total': total,
-#             'discount': discount,
-#             'pub_key': pub_key,
-#         })
-
-
-
 @login_required
 def checkout(request):
     cart = Cart.objects.get(user=request.user,status='inprogress')
     cart_detail= CartDetail.objects.filter(cart=cart)
     delivery_fee= DeliveryFee.objects.last().fee
     sub_total = cart.cart_total()
-    total = sub_total + delivery_fee
+    total = round(sub_total + delivery_fee,2)
     discount= 0
     pub_key = env('STRIP_API_KEY_PUBLISHABLE')
     if request.method == 'POST':
