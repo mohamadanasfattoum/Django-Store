33c4ad2720eadeaaa2c89be9246b49259b729e11
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-12 21:39:20 +0100

    update settings

--- project/settings.py
+++ project/settings.py
@@ -170,6 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
